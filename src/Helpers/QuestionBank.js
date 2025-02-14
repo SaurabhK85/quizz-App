ee0d91e7f41b758c9613c48f7ec758ce0a1e9b14
Saurabh Kumar <140698170+SaurabhK85@users.noreply.github.com>
2025-02-14 23:05:09 +0530

    Add files via upload

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,27 @@
+export const Questions = [
+    {
+        prompt: "What is 1+3?",
+        optionA: "24",
+        optionB: "21",
+        optionC: "4",
+        optionD: "2",
+        answer:  "C",
+
+    },
+    {
+        prompt: "Which is a Streaming Platfrom?",
+        optionA: "Twitter",
+        optionB: "Twitch",
+        optionC: "Google",
+        optionD: "Robinhood",
+        answer:  "B",
+    },
+    {
+        prompt: "Best Tech Youtube CHannel?",
+        optionA: "PedroTech",
+        optionB: "Dev Ed",
+        optionC: "Fireship",
+        optionD: "TwitchTim",
+        answer:  "A",
+    },
+];
